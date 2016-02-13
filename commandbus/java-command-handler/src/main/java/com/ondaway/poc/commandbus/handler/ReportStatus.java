ff6445b1c5dcfb19d6d2ec1b47c444ac2c908844
José Moreno <jomoespe@gmail.com>
2016-02-13 13:02:59 +0100

    Add commnad ReportStatus management

---
+++ commandbus/java-command-handler/src/main/java/com/ondaway/poc/commandbus/handler/ReportStatus.java
@@ -0,0 +1,8 @@
+package com.ondaway.poc.commandbus.handler;
+
+public class ReportStatus {
+    public String vehicle;    // `json:"vehicle"`
+    public boolean active;    // `json:"active"`
+    public boolean latitude;  // `json:"lat"`
+    public boolean longitude; // `json:"lng"`
+}
